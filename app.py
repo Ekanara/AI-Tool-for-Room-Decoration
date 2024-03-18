409b0d855092e30f3f6d65f052c1951e0b6d3230
Ekanari <vuanhkhoi0312@gmail.com>
2024-03-19 00:21:04 +0700

    update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from src.demo.download import check_and_download, download_all
 
 from src.demo.demo import create_demo_move, create_demo_appearance, create_demo_drag, create_demo_face_drag, create_demo_paste,create_demo_generate
 from src.demo.model import InteriorModels
+from style_generate import run_generate_style
 
 #import cv2
 import gradio as gr
@@ -23,7 +24,7 @@ with gr.Blocks(css="css.style") as demo:
     gr.Markdown(DESCRIPTION)
     with gr.Tabs():
         with gr.TabItem('Generate Image'):
-          create_demo_generate(model.run_generate_style)
+          create_demo_generate(run_generate_style)
         with gr.TabItem('Appearance Modulation'):
             create_demo_appearance(model.run_appearance)
         with gr.TabItem('Object Moving & Resizing'):
